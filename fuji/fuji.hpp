5dbc6e645b36f999aee947d79dc611b36cdc1f67
MarcoV <drake118@gmail.com>
2020-12-06 17:16:19 +0100

    Update fuji.hpp

--- fuji/fuji.hpp
+++ fuji/fuji.hpp
@@ -3,16 +3,14 @@
 // USAGE: set API in CMake
 
 #define FUJI_API_VULKAN 0
-#define FUJI_API_DX12   1
+#define FUJI_API_DX12 1
 
 #if FUJI_API != FUJI_API_VULKAN
 #error API not implemented
 #endif // FUJI_API
 
 // Api implementation here
-namespace fuji
-{
-    struct Context {
-
-    };
-}
+namespace fuji {
+	struct Context
+	{};
+} // namespace fuji
