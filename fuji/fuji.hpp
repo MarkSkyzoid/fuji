3fa8a314483e22d626b1d0b3c7708b4c4949aa37
MarcoV <drake118@gmail.com>
2020-12-06 17:36:52 +0100

    API setup and backend concept

--- fuji/fuji.hpp
+++ fuji/fuji.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 // USAGE: set API in CMake
-
 #define FUJI_API_VULKAN 0
 #define FUJI_API_DX12 1
 
@@ -9,8 +8,55 @@
 #error API not implemented
 #endif // FUJI_API
 
+#if defined(_WIN32)
+#define FUJI_WIN32
+// Pull in minimal Windows headers
+#if !defined(NOMINMAX)
+#define NOMINMAX
+#endif
+#if !defined(WIN32_LEAN_AND_MEAN)
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <Windows.h>
+#endif
+
+#include <stdint.h>
+
 // Api implementation here
 namespace fuji {
+	// Structures
+	struct PlatformData
+	{
+#ifdef FUJI_WIN32
+		HINSTANCE hinstance;
+		HWND hwnd;
+#else // FUJI_WIN32
+#error Platform not supported!
+#endif // FUJI_WIN32
+	};
+
+	struct ContextSettings
+	{
+		PlatformData platformData;
+
+		static constexpr uint32_t MAX_SWAPCHAIN_IMAGE_COUNT = 3;
+		uint32_t swapchainImageCount;
+	};
+
+	struct Backend; // Implementation specific. Needs to be defined in the implementation translation unit.
+
 	struct Context
-	{};
+	{
+		ContextSettings settings;
+		Backend& backend;
+	};
+
+	// Generic API
+	Context create_context(ContextSettings settings);
+	void destroy_context(Context& context);
+
+	// Resource API
+
+	// Command API
+
 } // namespace fuji
--- fuji/vulkan/fuji_vk.cpp
+++ fuji/vulkan/fuji_vk.cpp
@@ -3,6 +3,28 @@
 #include "../../ext/vk-bootstrap/src/VkBootstrap.h"
 #include "../../ext/VulkanMemoryAllocator/src/vk_mem_alloc.h"
 
-namespace fuji {} // namespace fuji
+namespace fuji {
+	// Structures
+	struct Backend
+	{
+		// #TODO: Vulkan objects here!
+	};
+
+	// Generic API
+
+	fuji::Context create_context(ContextSettings settings)
+	{
+		Context out_context = Context { settings, Backend {} };
+
+		settings.swapchainImageCount =
+		std::min(settings.swapchainImageCount, ContextSettings::MAX_SWAPCHAIN_IMAGE_COUNT);
+
+		out_context.settings = settings;
+
+		return out_context;
+	}
+
+	void destroy_context(Context& /*context*/) {}
+} // namespace fuji
 
 #endif // FUJI_API
