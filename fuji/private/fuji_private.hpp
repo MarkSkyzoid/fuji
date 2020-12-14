597439fd0a82357525acb9e850881a7573fc99e1
Marco Vallario <drake118@gmail.com>
2020-12-14 19:03:45 +0100

    Update fuji_private.hpp
    formatting (clang-format)

--- fuji/private/fuji_private.hpp
+++ fuji/private/fuji_private.hpp
@@ -7,8 +7,8 @@ namespace fuji {
 	bool validate_context_settings(const ContextSettings& settings);
 
 	template<typename T>
-	constexpr auto to_underlying_type(T enum_value) -> decltype(std::underlying_type_t<T>(enum_value)) 
+	constexpr auto to_underlying_type(T enum_value) -> decltype(std::underlying_type_t<T>(enum_value))
 	{
 		return static_cast<std::underlying_type_t<T>>(enum_value);
 	}
-}
+} // namespace fuji
